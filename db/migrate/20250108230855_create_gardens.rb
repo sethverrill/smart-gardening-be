82f183945edfd27db8a218f4e903195aea913ff5
sethverrill <verrill.seth@gmail.com>
2025-01-09 09:07:01 -0700

    fix: change hardiness_zone to zip_code

--- db/migrate/20250108230855_create_gardens.rb
+++ db/migrate/20250108230855_create_gardens.rb
@@ -2,7 +2,7 @@ class CreateGardens < ActiveRecord::Migration[8.0]
   def change
     create_table :gardens do |t|
       t.string :name
-      t.string :hardiness_zone
+      t.string :zip_code
       t.string :sunlight
       t.string :soil_type
       t.string :water_needs
