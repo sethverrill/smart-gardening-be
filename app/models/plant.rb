fb060006d24f6eaba2e1675a55ff33a8dd3ec239
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-09 17:55:32 -0700

    added model relations and added zip_code to the garden schema, it had hardiness zone

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,6 @@
 class Plant < ApplicationRecord
+  has_many :garden_plants, dependent: :destroy
+  has_many :gardens, through: :garden_plants
+
+  validates :name, :img_url, :description, presence: true
 end
