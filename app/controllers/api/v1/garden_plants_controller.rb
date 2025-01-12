2197d3793b03d9dfad8ca73024a2c09dd7f3b7c3
devklynch <devklynch@gmail.com>
2025-01-11 19:33:40 -0700

    adding methods to create gardenplants

--- app/controllers/api/v1/garden_plants_controller.rb
+++ app/controllers/api/v1/garden_plants_controller.rb
@@ -3,10 +3,42 @@ class Api::V1::GardenPlantsController < ApplicationController
     def update
         #add plant to a garden
         #get the garden id
-        binding.pry
         garden = Garden.find(params[:garden_id])
-        #inding.pry
-        #GardenPlant.create!(garden: garden, plant: plant)
+        #check to create the plant if it's not already made
+      
+        if !check_plant(params)
+            plant = Plant.create!(
+                name: params[:name],
+                img_url: params[:img_url],
+                description: params[:description]
+            )
+        else
+            plant = Plant.find_by(name: params[:name])
+        end
+
+        GardenPlant.create!(garden: garden, plant: plant)
+
+        #binding.pry
         render json: garden
     end
+
+    private
+    def plant_params
+        params.permit(:name, :img_url,:description)
+    end
+
+    def check_plant(params)
+        plant = Plant.find_by(name: params[:name])
+    end
+
+    def check_garden_plant(params)
+        gardenPlant
+    end
 end
+
+
+# tomato = Plant.create!(
+#   name: "Tomato",
+#   img_url: "https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg",
+#   description: "A popular vegetable that thrives in full sun."
+# )
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,5 +2,6 @@ class Plant < ApplicationRecord
   has_many :garden_plants, dependent: :destroy
   has_many :gardens, through: :garden_plants
 
-  validates :name, :img_url, :description, presence: true
+  validates :img_url, :description, presence: true
+  validates :name, uniqueness: true, presence: true
 end
