5cb2e086794969a929f7f1fc9f58f1a31b852b14
devklynch <devklynch@gmail.com>
2025-01-12 11:45:38 -0700

    Refactor and formatting updates

--- app/controllers/api/v1/garden_plants_controller.rb
+++ app/controllers/api/v1/garden_plants_controller.rb
@@ -4,15 +4,6 @@ class Api::V1::GardenPlantsController < ApplicationController
         garden = Garden.find(params[:garden_id])
       
         plant = find_or_create_plant(params)
-        # if !check_plant(params)
-        #     plant = Plant.create!(
-        #         name: params[:name],
-        #         img_url: params[:img_url],
-        #         description: params[:description]
-        #     )
-        # else
-        #     plant = Plant.find_by(name: params[:name])
-        # end
 
         unless check_garden_plant(garden,plant)
             GardenPlant.create!(garden: garden, plant: plant)
@@ -26,22 +17,18 @@ class Api::V1::GardenPlantsController < ApplicationController
         params.permit(:name, :img_url,:description)
     end
 
-    # def check_plant(params)
-    #     plant = Plant.find_by(name: params[:name])
-    # end
+    def find_or_create_plant(params)
+        plant = Plant.find_by(name: params[:name])
+        return plant if plant
+      
+        Plant.create!(
+          name: params[:name],
+          img_url: params[:img_url],
+          description: params[:description]
+        )
+    end
 
     def check_garden_plant(garden, plant)
         GardenPlant.exists?(garden: garden, plant:plant)
-    end
-
-    def find_or_create_plant(params)
-        if Plant.find_by(name: params[:name])
-            plant = Plant.find_by(name: params[:name])
-        else
-            plant = Plant.create!(
-                        name: params[:name],
-                        img_url: params[:img_url],
-                        description: params[:description])
-        end
     end
 end
--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,10 +1,4 @@
 class ErrorSerializer
-    def self.format_error(error_message)
-      {
-        message: error_message.message,
-        status: error_message.status_code
-      }
-    end
   
     def self.format_errors(messages)
       {
@@ -12,4 +6,4 @@ class ErrorSerializer
         errors: messages
       }
     end
-  end
+end
