895de3a5efcc6f43566832e6d52a2197f687dcd9
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-13 07:44:27 -0700

    create gardening, forgot to push it up yesterday

---
+++ app/controllers/api/v1/gardens_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::GardensController < ApplicationController
+  def create
+    garden = Garden.new(garden_params)
+    render json: { data: { id: garden.id } }, status: :created
+    
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name, :zip_code, :hardiness_zone, :sunlight, :soil_type, :water_needs, :purpose)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recommendation, only: [:index]
+      resources :gardens, only: [:create]
     end
   end
 end
