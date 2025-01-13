6fe02b0ab0f11f71db87348128e2cb1edc204c6e
Wally Wallace <jrvw001@gmail.com>
2025-01-13 15:30:40 -0700

    Add vercel to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3001', 'http://localhost:3000'
-  
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins "localhost:3000", "localhost:3001", "https://smart-gardening-fe.vercel.app/"
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+end
