e0a25b76ef5408ed5f8ccd09ef6e6c7cc86fa9ac
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-15 16:16:51 -0700

    added the garden index and route

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recommendation, only: [:index]
-      resources :gardens, only: [:show, :create, :update]  
+      resources :gardens, only: [:index, :show, :create, :update]  
       patch ':garden_id', to: 'garden_plants#update'
       delete '/gardens/:garden_id/plants/:plant_id', to: 'garden_plants#destroy'
     end
