52f7f646427d842f1b6bbbf5e8ef2aa6190ffe8d
Kaelin Salazar <kaelinpsalazar@gmail.com>
2025-01-15 11:43:47 -0700

    had to make a minor change to get the serializer to work

--- spec/requests/api/v1/gardens_controller_spec.rb
+++ spec/requests/api/v1/gardens_controller_spec.rb
@@ -69,7 +69,7 @@ RSpec.describe "Api::V1::Gardens", type: :request do
 
         expect(response).to have_http_status(:unprocessable_entity)
         parsed_response = JSON.parse(response.body, symbolize_names: true)
-        expect(parsed_response[:error]).to eq("Only one garden allowed at this time")
+        expect(parsed_response[:errors]).to eq("Only one garden allowed at this time")
       end
     end
 
